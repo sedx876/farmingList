2df2acb0b210cb6d254b97624f82d0490b84ffc9
sedx876 <swatkins876@gmail.com>
2020-09-21 10:14:30 -0400

    backend working

--- backend/app/controllers/items_controller.rb
+++ backend/app/controllers/items_controller.rb
@@ -1,2 +1,50 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :update, :destroy]
+
+  def index
+    @items =Item.all 
+    render json: ItemSerializer.new(@items) 
+  end
+
+  def show
+    render json: @item 
+  end
+
+  def create
+    @item = items.build(item_params)
+    if @item.save
+      render json: ItemSerializer.new(@item)
+    else
+      error_resp = {
+        error: @item.errors.full_messages.to_sentence
+      }
+      render json: error_resp 
+    end
+  end
+
+  def update
+    if @item.update(item_params) 
+      render json: ItemSerializer.new(@item)
+    else
+      error_resp = {
+        error: @item.errors.full_messages.to_sentence
+      }
+      render json: error_resp 
+    end
+  end
+
+  def destroy
+    @item.destroy
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name)
+  end
+
 end
