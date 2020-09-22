0da31e25a937e2268b37f69dae6e36ad2a222237
sedx876 <swatkins876@gmail.com>
2020-09-22 07:16:06 -0400

    deletingfiles to restructure app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import './App.css';
+import ItemsPage from './components/ItemsPage'
+import {connect} from 'react-redux'
+import {editItem, createItem, deleteItem} from './actions'
+import 'bootswatch/dist/sketchy/bootstrap.min.css'
 
-function App() {
+
+function App(props) {
+  const onStatusChange = (id, status) => {
+    props.dispatch(editItem(id, { status }));
+  };
+
+  const onCreateItem = ({ name }) => {
+    props.dispatch(createItem({ name }));
+  };
+
+  const onDeleteItem = (id) => {
+    props.dispatch(deleteItem(id));
+  };
+
   return (
     <div className="App">
       
+      <ItemsPage items={props.items} 
+      onStatusChange={onStatusChange} 
+      onCreateItem={onCreateItem} 
+      onDeleteItem={onDeleteItem} />
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    items: state.items
+  }
+}
+
+export default connect(mapStateToProps) (App);
